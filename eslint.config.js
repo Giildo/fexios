f269474f6c2a9345397cbf6bf7bafb2be5e4f723
Giildo <giildo.jm@gmail.com>
2025-05-05 21:26:25 +0200

    ⬆️ Upgrade

--- eslint.config.js
+++ eslint.config.js
@@ -31,8 +31,6 @@ export default tseslint.config(
 
   {
     rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       '@typescript-eslint/no-unused-expressions': [
         'error',
         {
